9f7a59904c099a81fcc6bdfeeadefbb7928b6191
dylanleungg <106607512+dylanleungg@users.noreply.github.com>
2022-06-24 10:18:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
-from xbbg import blp
+#from xbbg import blp
 import streamlit as st
-import seaborn as sns
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import plotly.express as px
@@ -28,27 +27,30 @@ date = '2014-12-31'
 
 #%% BBG Data Pull
 
-date_range = pd.bdate_range(date, dt.date.today())
-returns_data_ = pd.DataFrame(index = date_range)
+#date_range = pd.bdate_range(date, dt.date.today())
+#returns_data_ = pd.DataFrame(index = date_range)
 
-@st.cache
-def grabData():
-    dfs = []
-    for t in tickers:
-        df = blp.bdh(t, ['PX_LAST'], start_date = date, end_date = dt.date.today())
-        df.columns = df.columns.droplevel(0)
-        df.rename(columns = {'PX_LAST' : t}, inplace = True)
-        df.index = pd.to_datetime(df.index)
-        dfs.append(df)
+#@st.cache
+#def grabData():
+    #dfs = []
+    #for t in tickers:
+        #df = blp.bdh(t, ['PX_LAST'], start_date = date, end_date = dt.date.today())
+        #df.columns = df.columns.droplevel(0)
+        #df.rename(columns = {'PX_LAST' : t}, inplace = True)
+        #df.index = pd.to_datetime(df.index)
+        #dfs.append(df)
 
-    returns_data_ = pd.concat(dfs, axis=1)
-    return returns_data_
+    #returns_data_ = pd.concat(dfs, axis=1)
+    #return returns_data_
 
-returns_data_ = grabData()
+#returns_data_ = grabData()
 
 #%%Copy results
 
-returns_data = returns_data_.copy().sort_index()
+returns_data = pd.read_csv('returns_data.csv', index_col = ['Unnamed: 0'])
+#returns_data = returns_data_.copy().sort_index()
+
+returns_data.index = pd.to_datetime(returns_data.index)
 
 rules = {'AAPL US EQUITY': 'last', 
            'BABA US EQUITY': 'last', 
@@ -175,6 +177,7 @@ long_optimal_weights.columns = lst
 
 #%% Calculate Returns
 
+#Create individual dfs for each portfolio
 actual_returns = returns_data.loc[:, (~returns_data.columns.str.contains('PREDICTED')) & 
                                   (returns_data.columns.str.contains('RETURN')) & 
                                   (~returns_data.columns.str.contains('INDEX'))]
@@ -183,10 +186,23 @@ ls_returns = actual_returns.copy()
 equal_weight = actual_returns.copy()
 equal_weight = equal_weight.loc[equal_weight.index.year.isin(years[1:])] #Filter to match long and l/s index 
 
+
+#Cumulative single stock returns for dashboard chart
+stocks = returns_data.columns[11:22]
+
+for stock in stocks:
+    share_prices[stock + ' CUM RETURN'] = (1 + returns_data[stock]).cumprod() - 1
+
+share_prices = share_prices.loc[:, share_prices.columns.str.contains('CUM RETURN')]
+
+
+#SPX Returns DF
 spx_returns = pd.DataFrame(returns_data.iloc[:,-1])
 spx_returns = spx_returns.loc[spx_returns.index.year.isin(years[1:])] #Filter to match long and l/s index 
-spx_returns['CUM RETURN'] = (1 + spx_returns['SPX INDEX RETURN']) * ((1 + spx_returns['SPX INDEX RETURN']).shift(1)) - 1
+spx_returns['SPX CUM RETURN'] = (1 + spx_returns['SPX INDEX RETURN']).cumprod() - 1
 
+
+#Applying weights to portfolios
 long_only_returns = long_only_returns.merge(long_optimal_weights, left_on = long_only_returns.index.year, right_index = True)
 ls_returns = ls_returns.merge(ls_optimal_weights, left_on = ls_returns.index.year, right_index = True)
 long_only_returns.drop(columns = 'key_0', inplace = True)
@@ -196,15 +212,17 @@ for ele in lst:
     equal_weight[ele] = n / 100 
 
 
-#Is there a more efficient way to do this?
+#Calculating cumulative returns, Sharpe Ratios, Annualized Volatility
+
+#Is there a more efficient way to do W.A across >2 columns?
 
 long_only_returns['LONG PORTFOLIO RETURN'] = (long_only_returns['AAPL  RETURN'] * long_only_returns['AAPL']) + (long_only_returns['BABA  RETURN'] * long_only_returns['BABA']) + (long_only_returns['DIS  RETURN'] * long_only_returns['DIS']) + (long_only_returns['LULU  RETURN'] * long_only_returns['LULU']) + (long_only_returns['PSX  RETURN'] * long_only_returns['PSX']) + (long_only_returns['SPGI  RETURN'] * long_only_returns['SPGI']) + (long_only_returns['LMT  RETURN'] * long_only_returns['LMT']) + (long_only_returns['TXN  RETURN'] * long_only_returns['TXN']) + (long_only_returns['V  RETURN'] * long_only_returns['V']) + (long_only_returns['MCD  RETURN'] * long_only_returns['MCD'])     
 ls_returns['LS PORTFOLIO RETURN'] = (ls_returns['AAPL  RETURN'] * ls_returns['AAPL']) + (ls_returns['BABA  RETURN'] * ls_returns['BABA']) + (ls_returns['DIS  RETURN'] * ls_returns['DIS']) + (ls_returns['LULU  RETURN'] * ls_returns['LULU']) + (ls_returns['PSX  RETURN'] * ls_returns['PSX']) + (ls_returns['SPGI  RETURN'] * ls_returns['SPGI']) + (ls_returns['LMT  RETURN'] * ls_returns['LMT']) + (ls_returns['TXN  RETURN'] * ls_returns['TXN']) + (ls_returns['V  RETURN'] * ls_returns['V']) + (ls_returns['MCD  RETURN'] * ls_returns['MCD'])              
 equal_weight['EQW PORTFOLIO RETURN'] = (equal_weight['AAPL  RETURN'] * equal_weight['AAPL']) + (equal_weight['BABA  RETURN'] * equal_weight['BABA']) + (equal_weight['DIS  RETURN'] * equal_weight['DIS']) + (equal_weight['LULU  RETURN'] * equal_weight['LULU']) + (equal_weight['PSX  RETURN'] * equal_weight['PSX']) + (equal_weight['SPGI  RETURN'] * equal_weight['SPGI']) + (equal_weight['LMT  RETURN'] * equal_weight['LMT']) + (equal_weight['TXN  RETURN'] * equal_weight['TXN']) + (equal_weight['V  RETURN'] * equal_weight['V']) + (equal_weight['MCD  RETURN'] * equal_weight['MCD'])              
 
-long_only_returns['LONG CUM RETURN'] = ((1 + long_only_returns['LONG PORTFOLIO RETURN']) * (1 + long_only_returns['LONG PORTFOLIO RETURN']).shift(1)) - 1
-ls_returns['LS CUM RETURN'] = ((1 + ls_returns['LS PORTFOLIO RETURN']) * (1 + ls_returns['LS PORTFOLIO RETURN']).shift(1)) - 1
-equal_weight['EQW CUM RETURN'] = ((1 + equal_weight['EQW PORTFOLIO RETURN']) * (1 + equal_weight['EQW PORTFOLIO RETURN']).shift(1)) - 1
+long_only_returns['LONG CUM RETURN'] = (1 + long_only_returns['LONG PORTFOLIO RETURN']).cumprod() - 1
+ls_returns['LS CUM RETURN'] = (1 + ls_returns['LS PORTFOLIO RETURN']).cumprod() - 1
+equal_weight['EQW CUM RETURN'] = (1 + equal_weight['EQW PORTFOLIO RETURN']).cumprod() - 1
 
 long_only_returns['LONG 3M PORT SD'] = long_only_returns['LONG PORTFOLIO RETURN'].rolling(window = 3, min_periods = 3).std()
 ls_returns['LS 3M PORT SD'] = ls_returns['LS PORTFOLIO RETURN'].rolling(window = 3, min_periods = 3).std()
@@ -234,25 +252,27 @@ finalDF = longDF.merge(lsDF, left_index = True, right_index = True).merge(eqwDF,
 import plotly.io as pio
 pio.renderers.default='browser' #to launch graphs in browser when running in Spyder
 
+
 ticker_select = st.multiselect('Select a ticker.', share_prices.columns[0:10])
 st.write('You selected', ticker_select)
 
-start = st.date_input('Start Date', share_prices.index)
-end = st.date_input('End Date', share_prices.index)
+start = st.date_input('Pick Your Start Date')
+end = st.date_input('Pick Your End Date')
 
 fig_one = px.line(share_prices.loc[start:end], title = 'Share Price Performance Over Time')
 st.plotly_chart(fig_one)
 
-fig_two = px.line(finalDF.loc[start:end][['LONG CUM RETURN', 'LS CUM RETURN', 'EQW CUM RETURN']], title = 'Cumulative Returns')
+fig_two = px.line(finalDF.loc[start:end][['LONG CUM RETURN', 'LS CUM RETURN', 'EQW CUM RETURN']], title = 'Portfolio Cumulative Returns')
 st.plotly_chart(fig_two)
 
 fig_thr = px.line(finalDF.loc[start:end][['LONG 3M SHARPE RATIO', 'LS 3M SHARPE RATIO', 'EQW 3M SHARPE RATIO']], title = '3 Month Rolling Sharpe Ratio')
 st.plotly_chart(fig_thr)
 
 
-portfolio_select = st.select('Select a Portfolio', ['LONG', 'LS', 'EQW'])
+portfolio_select = st.selectbox('Select a Portfolio', ['LONG', 'LS', 'EQW'], index = 0)
 
 fig_four = px.line(finalDF.loc[start:end][portfolio_select + ' VOLATILITY'], title = 'Annualized Volatility')
 st.plotly_chart(fig_four)
+
 
 st.write(allBetasDF)
